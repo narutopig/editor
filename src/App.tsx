9edb034957912aece7a6bf5cec1c8ea0cac7b170
pogtato <69479556+narutopig@users.noreply.github.com>
2021-10-27 16:46:18 -0700

    Organizational changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,11 @@
 import React from "react";
-import ReactMarkdown from "react-markdown";
-import CodeEditor from "@uiw/react-textarea-code-editor";
 import "./App.css";
+import Editor from "./components/Editor";
 
 function App() {
-  const [code, setCode] = React.useState("*Hello World!*");
-
   return (
-    <div className="App">
-      <div>
-        <CodeEditor
-          value={code}
-          language="markdown"
-          placeholder="Enter markdown code here"
-          onChange={(evn) => setCode(evn.target.value)}
-          padding={15}
-          style={{
-            fontSize: 12,
-            backgroundColor: "#f5f5f5",
-            fontFamily:
-              "ui-monospace,SFMono-Regular,SF Mono,Consolas,Liberation Mono,Menlo,monospace",
-            height: "100vh",
-            width: "50vw",
-          }}
-        />
-      </div>
-      <div>
-        <ReactMarkdown>{code}</ReactMarkdown>
-      </div>
+    <div>
+      <Editor />
     </div>
   );
 }
---
+++ src/components/Editor.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+import ReactMarkdown from "react-markdown";
+import CodeEditor from "@uiw/react-textarea-code-editor";
+import "../App.css";
+
+function Editor() {
+  const [code, setCode] = React.useState("*Hello World!*");
+
+  return (
+    <div className="editor">
+      <div>
+        <CodeEditor
+          value={code}
+          language="markdown"
+          placeholder="Enter markdown code here"
+          onChange={(evn) => setCode(evn.target.value)}
+          padding={15}
+          style={{
+            fontSize: 12,
+            backgroundColor: "#f5f5f5",
+            fontFamily:
+              "ui-monospace,SFMono-Regular,SF Mono,Consolas,Liberation Mono,Menlo,monospace",
+            height: "100vh",
+            width: "50vw",
+          }}
+        />
+      </div>
+      <div>
+        <ReactMarkdown>{code}</ReactMarkdown>
+      </div>
+    </div>
+  );
+}
+
+export default Editor;
